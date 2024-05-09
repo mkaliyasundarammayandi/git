2d0c4dc5ee27f10f2f2eeba26c979606c36ff9a7
Kaliya sundaram mayandi M <143984598+mkaliyasundarammayandi@users.noreply.github.com>
2024-05-09 14:38:13 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication13;
+
+/**
+ *
+ * @author Admin
+ */
+public class Hello {
+    
+    public static void main(){
+        System.out.println("hello world");
+    }
+}
